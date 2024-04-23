4f4ecc151f96baf6c9fd629ef69b02e50593f20c
Marya <masha.smelkowa@yandex.ru>
2024-04-23 20:21:54 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int tiket = 6000;
+        int bonus = 20;
+        int miles = tiket / bonus;
+        System.out.println (" При покупке билета стоимостью " + tiket + " руб. Ваш бонус составит " + miles + " бонусных миль ");
+
+
+    }
+}
